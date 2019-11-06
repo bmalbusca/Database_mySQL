76eba9892c8d82c446108652c65822ba5ae08b9c
Bruno Figueiredo <brunocfigueiredo@tecnico.ulisboa.pt>
2019-11-06 23:44:08 +0000

    data for Q5

--- create_database.sql
+++ create_database.sql
@@ -253,10 +253,10 @@ INSERT INTO client VALUES ('123456789','John','1985-02-07','Av almirante reis','
 INSERT INTO client VALUES ('123456780','Robert','1985-03-07','Av almirante reis','Lisboa', '1000-048','Male',20);
 INSERT INTO client VALUES ('123456781','Charles','1988-03-06','Av almirante reis','Lisboa', '1000-020','Male',25);
 
-INSERT INTO appointment VALUES ('123400000','2008-01-01 00:00:01','Its a bad situation','123456789');
+INSERT INTO appointment VALUES ('123400000','2008-01-01 00:00:15','Its a bad situation','123456789');
 INSERT INTO appointment VALUES ('123400000','2008-01-02 00:00:01','Its a not bad situation','123456780');
 
-INSERT INTO consultation VALUES ('123400000','2008-01-01 00:00:01','not ok','ok','not ok','ok');
+INSERT INTO consultation VALUES ('123400000','2008-01-01 00:00:15','not ok','gingivitis','not ok','ok');
 INSERT INTO consultation VALUES ('123400000','2008-01-02 00:00:01','ok','gingivitis','ok','ok');
 
 INSERT INTO phone_number_client VALUES ('123456789', 910000000);
@@ -270,22 +270,21 @@ INSERT INTO supervision_report VALUES ('123400001','2008-01-02 00:00:01','insuff
 
 INSERT INTO trainee_doctor VALUES ('123400002','123400000');
 INSERT INTO supervision_report VALUES ('123400002','2008-01-02 00:00:03','good', 3);
- 
 
 
-
--- SELECT client.name, client.city, client.VAT, phone_number_client.phone from client, phone_number_client ,employee, consultation 
--- NATURAL JOIN (
---	appointment 
-		
---		WHERE appointment.VAT_client =  client.VAT   AND   WHERE cons 
---		)
-
-SELECT  MAX(appointment.date_timestamp)from client as c1, appointment
-	NATURAL JOIN client ;
+INSERT INTO diagnostic_code VALUES ('ICD-10-CM', 'He is a dead men'); 
+INSERT INTO diagnostic_code VALUES ('ICD-00-CM', 'He is  a zombie'); 
 
 
+INSERT INTO consultation_diagnostic VALUES('123400000','2008-01-02 00:00:01','ICD-10-CM'); 
+INSERT INTO consultation_diagnostic VALUES('123400000','2008-01-01 00:00:15','ICD-00-CM'); 
 
+
+INSERT INTO medication VALUES ('cacao' , 'choc');
+INSERT INTO medication VALUES ('nutela' , 'choc');
+
+INSERT INTO prescription VALUES ('cacao','choc','123400000','2008-01-02 00:00:01','ICD-10-CM','100ml','All in your ass');
+INSERT INTO prescription VALUES ('cacao','choc','123400000','2008-01-01 00:00:15','ICD-00-CM','1L','All in your vein');
 
 
 
