22b7460bceb9a72f01f7ec6f3999162dde66b285
Bruno Figueiredo <brunocfigueiredo@tecnico.ulisboa.pt>
2019-11-07 22:01:19 +0000

    more progress at u3

--- u3.sql
+++ u3.sql
@@ -1,6 +1,16 @@
 USE dental_clinic;
 
-DELETE FROM  employee as e
-WHERE e.name LIKE 'Jane Sweettooth';
+-- DELETE FROM  employee as e
+-- WHERE e.name LIKE 'Jane Sweettooth';
+
+SELECT COUNT(p.name) FROM procedure_in_consultation as p, 
+	(SELECT pc.name, pc.VAT_doctor FROM procedure_in_consultation as pc 
+		WHERE pc.VAT_doctor =	(SELECT e.VAT FROM employee as e
+			WHERE e.name LIKE 'Jane Sweettooth')) as pname
+WHERE (p.VAT_doctor <> pname.VAT_doctor AND p.name = pname.name)
+GROUP BY p.name;
+
+
+
 
 
