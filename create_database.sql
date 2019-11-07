6b6e47953d2805f6b32e6e7562c4f5abf5351795
Pedro Costa <pcosta91@gmail.com>
2019-11-07 00:07:15 +0000

    teste Q3 + data

--- create_database.sql
+++ create_database.sql
@@ -289,7 +289,17 @@ INSERT INTO medication VALUES ('nutela' , 'choc');
 INSERT INTO prescription VALUES ('cacao','choc','123400000','2008-01-02 00:00:01','ICD-10-CM','100ml','All in your ass');
 INSERT INTO prescription VALUES ('cacao','choc','123400000','2008-01-01 00:00:15','ICD-00-CM','1L','All in your vein');
 
-
+SELECT Cl.name, Cl.city, Cl.VAT FROM client as Cl, 
+	(SELECT con.SOAP_O, res.VAT_client FROM consultation as con 
+		JOIN
+		(SELECT * FROM appointment as A
+			INNER JOIN(
+					SELECT MAX(A.date_timestamp) MAX_ts, A.VAT_client VAT
+					FROM appointment as A NATURAL JOIN consultation as C
+					GROUP BY A.VAT_client) as Filter
+			ON A.VAT_client = Filter.VAT AND A.date_timestamp = Filter.MAX_ts ) as res
+		ON con.VAT_doctor =res.VAT_doctor AND con.date_timestamp = res.date_timestamp) as t
+WHERE Cl.VAT = t.VAT_client AND (t.SOAP_O LIKE '%gingivitis%' OR t.SOAP_O LIKE '%periodontitis%');
 
 
 
