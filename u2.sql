b563b1994280cdf339de1e6f2b428e4cd53c3b67
Bruno Figueiredo <brunocfigueiredo@tecnico.ulisboa.pt>
2019-11-07 17:33:59 +0000

    u1 e u2 done

---
+++ u2.sql
@@ -0,0 +1,11 @@
+USE dental_clinic;
+
+
+UPDATE  employee 
+SET salary = salary + (salary * 0.05)
+WHERE employee.VAT IN (
+	SELECT ap.VAT_doctor FROM appointment as ap
+    GROUP by (ap.VAT_doctor)
+    HAVING COUNT(DISTINCT ap.VAT_doctor, ap.date_timestamp) > 2
+
+);
