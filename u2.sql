a12a21bc578fe29f65ddf986742263e503e1e3f7
Pedro Costa <pcosta91@gmail.com>
2019-11-08 18:24:36 +0000

    arranjar

--- u2.sql
+++ u2.sql
@@ -6,6 +6,6 @@ SET salary = salary + (salary * 0.05)
 WHERE employee.VAT IN (
 	SELECT ap.VAT_doctor FROM appointment as ap
     GROUP by (ap.VAT_doctor)
-    HAVING COUNT(DISTINCT ap.VAT_doctor, ap.date_timestamp) > 2
+    HAVING COUNT(DISTINCT ap.VAT_doctor, ap.date_timestamp) > 100
 
 );
--- u3.sql
+++ u3.sql
@@ -1,25 +1,22 @@
 USE dental_clinic;
 
 
-DELETE FROM `procedure`as p WHERE (p.name= (SELECT distinct(pc.name) AS pname FROM procedure_in_consultation as pc 
-									WHERE pc.VAT_doctor =	(SELECT e.VAT FROM employee as e
-										WHERE e.name LIKE 'Jane Sweettooth')) and  NOT EXISTS(
-
+DELETE FROM `procedure`as p 
+WHERE (p.name= (SELECT distinct(pc.name) AS pname 
+	FROM procedure_in_consultation as pc 
+	WHERE pc.VAT_doctor =(SELECT e.VAT 
+		FROM employee as e
+		WHERE e.name LIKE 'Jane Sweettooth')) AND
+		NOT EXISTS(
 SELECT t2.pname FROM (SELECT p.name, p.VAT_doctor FROM procedure_in_consultation as p
-				WHERE p.VAT_doctor != (SELECT e.VAT FROM employee as e
-				WHERE e.name LIKE 'Jane Sweettooth') )as t
-                INNER JOIN  (SELECT distinct(pc.name) AS pname FROM procedure_in_consultation as pc 
-									WHERE pc.VAT_doctor =	(SELECT e.VAT FROM employee as e
-										WHERE e.name LIKE 'Jane Sweettooth')) as t2
-                                        ON t.name = t2.pname
-                                        
-
-
+	WHERE p.VAT_doctor != (SELECT e.VAT FROM employee as e
+	WHERE e.name LIKE 'Jane Sweettooth') )as t
+	INNER JOIN  (SELECT distinct(pc.name) AS pname 
+		FROM procedure_in_consultation as pc 
+		WHERE pc.VAT_doctor =	(SELECT e.VAT FROM employee as e
+			WHERE e.name LIKE 'Jane Sweettooth')) as t2
+			ON t.name = t2.pname
 ));
 
 DELETE FROM  employee as e
-WHERE e.name LIKE 'Jane Sweettooth';                
-		
-
-
-
+WHERE e.name LIKE 'Jane Sweettooth';
