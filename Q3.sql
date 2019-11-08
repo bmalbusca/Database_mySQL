fd8b920fb20dd8228f03bc42d0183b9a275e9ffa
Pedro Costa <pcosta91@gmail.com>
2019-11-08 12:47:33 +0000

    q1 e q2 + arranjar

--- Q3.sql
+++ Q3.sql
@@ -4,8 +4,8 @@ SELECT Cl.name, Cl.city, Cl.VAT FROM client as Cl,
 		(SELECT * FROM appointment as A
 			INNER JOIN(
 					SELECT MAX(A.date_timestamp) MAX_ts, A.VAT_client VAT
-					FROM appointment as A NATURAL JOIN consultation as C
+					FROM 	appointment as A NATURAL JOIN consultation as C
 					GROUP BY A.VAT_client) as Filter
-			ON A.VAT_client = Filter.VAT AND A.date_timestamp = Filter.MAX_ts ) as res
-		ON con.VAT_doctor =res.VAT_doctor AND con.date_timestamp = res.date_timestamp) as t
-WHERE Cl.VAT = t.VAT_client AND (t.SOAP_O LIKE '%gingivitis%' OR t.SOAP_O LIKE '%periodontitis%');
+			ON 	A.VAT_client = Filter.VAT AND A.date_timestamp = Filter.MAX_ts ) as res
+		ON 	con.VAT_doctor =res.VAT_doctor AND con.date_timestamp = res.date_timestamp) as t
+WHERE 	Cl.VAT = t.VAT_client AND (t.SOAP_O LIKE '%gingivitis%' OR t.SOAP_O LIKE '%periodontitis%');
